d8fff2fe5e3e577f0f64b027509ea294dc52c7b2
Jeff Tipps <jeff.tipps@ni.com>
2023-10-28 18:02:52 -0500

    can pass slice to variadic function

--- Chapter06/variadic/main.go
+++ Chapter06/variadic/main.go
@@ -8,6 +8,8 @@ func main() {
 	severalStrings("a", "b")
 	severalStrings("a", "b", "c", "d", "e")
 	severalStrings()
+	var strings []string = []string {"j", "e", "f", "f"}
+	severalStrings(strings...)
 }
 
 func severalStrings(strings ...string) {	// strings is a slice of type `string`
