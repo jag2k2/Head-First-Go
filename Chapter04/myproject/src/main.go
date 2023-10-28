e707f8d974a38151544f2175f0835dbaf0d20c88
Jeff Tipps <jeff.tipps@ni.com>
2023-10-28 10:06:57 -0500

    adding keyboard package

--- Chapter04/myproject/src/main.go
+++ Chapter04/myproject/src/main.go
@@ -3,12 +3,23 @@ package main
 import "fmt"
 import "log"
 import "myproject/src/greetings"
+import "myproject/src/keyboard"
 
 func main() {
-	names := []string{"Gladys", "Samantha", "Darrin"}
-	messages, err := greetings.Hellos(names)
+	message, err := greetings.Hello("Gladys")
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(messages)
+	fmt.Println(message, "Enter a grade: ")
+	grade, err := keyboard.GetFloat()
+	if err != nil {
+		log.Fatal(err)
+	}
+	var status string
+	if grade >= 60 {
+		status = "passing"
+	} else {
+		status = "failing"
+	}
+	fmt.Println("A Grade of", grade, "is", status)
 }
