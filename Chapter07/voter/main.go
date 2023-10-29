ef744b83b02ce0099d94a8413759a844144cb55e
Jeff Tipps <jeff.tipps@ni.com>
2023-10-28 19:03:24 -0500

    adding voter application

--- Chapter07/voter/main.go
+++ Chapter07/voter/main.go
@@ -5,10 +5,14 @@ import "log"
 import "voter/datafile"
 
 func main() {
-	contents, err := datafile.ReadStrings("data.txt")
+	contents, err := datafile.ReadStrings("votes.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(contents)
+	var ranks map[string]int = make(map[string]int)
+	for _, line := range contents {
+		ranks[line]++
+	}
+	fmt.Println(ranks)
 }
 
