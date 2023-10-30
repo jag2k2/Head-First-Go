3bbdf57f47806251c20b7cf792ba00d5e5977c77
Jeff Tipps <jeff.tipps@ni.com>
2023-10-29 22:19:53 -0500

    use pointers with struct

--- Chapter08/subscriber/main.go
+++ Chapter08/subscriber/main.go
@@ -8,25 +8,29 @@ type subscriber struct {
 	active 	bool
 }
 
-func printInfo(sub subscriber) {
+func main() {
+	var subscriber1 *subscriber = defaultSubscriber("Aman Singh")
+	applyDiscount(subscriber1)
+	printInfo(subscriber1)
+
+	subscriber2 := defaultSubscriber("Beth Ryan")
+	printInfo(subscriber2)
+}
+
+func printInfo(sub *subscriber) {
 	fmt.Println("Name:", sub.name)
 	fmt.Println("Monthly rate:", sub.rate)
 	fmt.Println("Active?", sub.active)
 }
 
-func defaultSubscriber(name string) subscriber {
+func defaultSubscriber(name string) *subscriber {
 	var sub subscriber
 	sub.name = name
 	sub.rate = 5.99
 	sub.active = true
-	return sub
+	return &sub
 }
 
-func main() {
-	subscriber1 := defaultSubscriber("Aman Singh")
-	subscriber1.rate = 4.99
-	printInfo(subscriber1)
-
-	subscriber2 := defaultSubscriber("Beth Ryan")
-	printInfo(subscriber2)
+func applyDiscount(sub *subscriber) {
+	sub.rate = 4.99
 }
