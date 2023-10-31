cc0a427b6e932cf8a747fcb7cf9e704a9717e40f
Jeff Tipps <jeff.tipps@ni.com>
2023-10-31 13:13:29 -0500

    added end of chpt exercise

---
+++ Chapter08/landmark/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+import "landmark/geo"
+
+func main() {
+	location := geo.Landmark{}
+	location.Name = "The Googleplex"
+	location.Latitude = 37.42
+	location.Longitude = -122.08
+	fmt.Println(location)
+}
