cb676dd314fba97f2c15774494c922282f906ea3
Jeff Tipps <jeff.tipps@ni.com>
2023-11-02 18:22:17 -0500

    added method

--- Chapter11/robot/main.go
+++ Chapter11/robot/main.go
@@ -7,9 +7,12 @@ import (
 
 func main() {
 	var robot interfaces.Robot = transformers.NewAutobot("Optimus Prime")
-	robot.MakeSound()
-	robot.Walk()
-	robot = transformers.NewDecepticon("Starscream")
+	doRobotStuff(robot)
+	var robot2 interfaces.Robot = transformers.NewDecepticon("Starscream")
+	doRobotStuff(robot2)
+}
+
+func doRobotStuff(robot interfaces.Robot) {
 	robot.MakeSound()
 	robot.Walk()
 }
