da0daa8957139a4ee678bfda08cbaefe054e7ef4
Jeff Tipps <jeff.tipps@ni.com>
2023-11-06 14:53:27 -0600

    added sleep to basicchan

--- Chapter13/basicchan/main.go
+++ Chapter13/basicchan/main.go
@@ -1,14 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func greeting(myChannel chan string) {
+	fmt.Println("Greeting initiated.  Will respond in 3 seconds:")
+	time.Sleep(3 * time.Second)
 	myChannel <- "hi"
 }
 
 func main() {
 	myChannel := make(chan string)
 	go greeting(myChannel)
+	fmt.Println("Waiting for greeting:")
 	response := <-myChannel
 	fmt.Println(response)
 }
