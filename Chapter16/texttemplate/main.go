ef7c5144a5cf460b8a9851d37c0d759b7bdc6b61
Jeff Tipps <jeff.tipps@ni.com>
2023-11-06 17:57:27 -0600

    finished texttemplate examples

--- Chapter16/texttemplate/main.go
+++ Chapter16/texttemplate/main.go
@@ -19,8 +19,27 @@ func executeTemplate(text string, data interface{}) {
 	check(err)
 }
 
+type Part struct {
+	Name  string
+	Count int
+}
+
 func main() {
 	templateText := "Dot is: {{.}}\n"
 	executeTemplate(templateText, "ABC")
 	executeTemplate(templateText, 123.5)
+
+	templateText = "start {{if .}}Dot is true!{{end}} finish\n"
+	executeTemplate(templateText, true)
+	executeTemplate(templateText, false)
+
+	templateText = "Before loop: {{.}}\n{{range .}}In loop: {{.}}\n{{end}}After loop: {{.}}\n" //first and last do contain slice.  middle dot contains element
+	executeTemplate(templateText, []string{"do", "re", "mi"})
+
+	templateText = "Prices:\n{{range .}}${{.}}\n{{end}}"
+	executeTemplate(templateText, []float64{1.25, 0.99, 27})
+
+	templateText = "Name: {{.Name}}\tCount: {{.Count}}\n"
+	executeTemplate(templateText, Part{Name: "Fuses", Count: 5})
+	executeTemplate(templateText, Part{Name: "Cables", Count: 2})
 }
